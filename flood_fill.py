308eab42228f3a4db85f740de01c4c45954ef06f
marielacordeiro <mariela_pontes_corde@dellteam.com>
2022-06-01 15:55:40 -0300

    funcionando <3

--- flood_fill.py
+++ flood_fill.py
@@ -1,75 +1,101 @@
-lines = []
-curr_size = 1
-
-with open("casos-cohen/caso04.txt") as f:
-    for line in f:
-        lines.append(list(line.strip()))
+import os
+import sys
+sys.setrecursionlimit(50000)
 
 
-def print_field():
-    # this function will print the content of the array to the console
-    for y in range(len(lines)):
-        for x in range(len(lines[0])):
+def read_file(test_case: str):
+    lines = []
+    with open(f'casos-cohen/{test_case}') as f:
+        for line in f:
+            lines.append(list(line.strip()))
+    return lines
+
+
+def find_players(list):
+    players = {}
+    # this function will find the players and keep their coordinates
+    for x in range(len(list)-1):
+        for y in range(len(list[0])):
+            if list[x][y].isnumeric():
+                number = int(list[x][y])
+                players[number] = x, y
+            if y == len(list[0])-1:
+                break
+    return players
+
+
+def print_field(list):
+    for y in range(len(list)):
+        for x in range(len(list[0])):
             # value by column and row
-            print(lines[y][x], end=' ')
-            if x == len(lines[0])-1:
+            print(list[y][x], end=' ')
+            if x == len(list[0])-1:
                 # print a new line at the end of each row
                 print('\n')
 
 
-vis = [[0 for _ in range(len(lines[0]))] for _ in range(len(lines))]
-
-
-def flood_fill(x, y, dot, door_keys):
-    # we need the x and y of the start position, the old value,
-    # and the new value
-
-    # the flood fill has 4 parts
-
-    # firstly, make sure the x and y are inbounds
-    if x < 0 or x >= len(lines[0]) or y < 0 or y >= len(lines):
+def flood_fill(x, y, dot, door_keys, doors):
+    # make sure the x and y are inbounds
+    if x < 0 or x >= len(lines) or y < 0 or y >= len(lines[0]):
         return
 
-    # secondly, check if the current position equals the old value
-    if lines[x][y] != dot:
-        if lines[x][y].isalpha():
-            if lines[x][y].islower():
-                door_keys.add(lines[x][y])
-            else:
-                lines[x][y].lower() not in door_keys
-                return
-        else:
-            return
-
+    # check if the current position has already been visited
     if vis[x][y] == 1:
         return
 
-    # thirdly, set the current position to the new value
+    # check if the current position equals the desirable value
+    if lines[x][y] != dot:
+        position = lines[x][y]
+        if position.isalpha():
+            if position.islower():
+                door_keys.add(position)
+                if position.upper() in doors.keys():
+                    vis[x][y] = 0
+                    flood_fill(doors[position.upper()][
+                        0], doors[position.upper()][1], dot, door_keys, doors)
+            elif position.lower() not in door_keys:
+                doors[position] = x, y
+                return
+        elif position.isnumeric():
+            pass
+        else:
+            return
+
+    # thirdly, set the current position to visited and increment the positions visited
     vis[x][y] = 1
     global curr_size
     curr_size += 1
 
     # fourthly, attempt to fill the neighboring positions
-    flood_fill(x+1, y, dot, door_keys)
-    flood_fill(x-1, y, dot, door_keys)
-    flood_fill(x, y+1, dot, door_keys)
-    flood_fill(x, y-1, dot, door_keys)
+    flood_fill(x+1, y, dot, door_keys, doors)
+    flood_fill(x-1, y, dot, door_keys, doors)
+    flood_fill(x, y+1, dot, door_keys, doors)
+    flood_fill(x, y-1, dot, door_keys, doors)
 
     return curr_size
 
 
 if __name__ == "__main__":
-    # print field before the flood fill
-    # print_field()
+    print(sys.getrecursionlimit())
+    results = {}
+    for file in os.listdir('casos-cohen'):
+        if file not in results:
+            results[file] = []
+        lines = read_file(file)
+        players = find_players(lines)
+        for x, y in players.items():
+            curr_size = 0
+            door_keys = set()
+            doors = {}
+            vis = [[0 for _ in range(len(lines[0]))]
+                   for _ in range(len(lines))]
+            path_size = flood_fill(y[0], y[1], ".", door_keys, doors)
+            results[file].append((x, path_size))
+            print()
+            print(
+                f'caso de teste: {file} \t player: {x} \t casinhas exploradas: {path_size}')
 
-    door_keys = set()
-
-    # cases 15, 16
-    points = flood_fill(16, 15, ".", door_keys)
-
-    print("Doing flood fill with ")
-
-    # print the field after the flood fill
-    # print_field()
-
-    print(points)
+    """ print()
+    [print(f'caso de teste: {k} \t player: {v} \t casinhas exploradas: {v}')
+     for k, v in results.items()]
+    print() """
